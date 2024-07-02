acf8aa68754ac608e3695d0224ce5767b86436c1
huangsheng <1490085442@qq.com>
2024-07-02 15:22:05 +0800

    初始提交

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+export default function Home() {
+  return (
+      <>
+       <button className="button">
+           立即登陆
+       </button>
+
+
+        <style jsx>{`
+          .button{
+            color: crimson;
+            font-size: 48px;
+            font-weight: bold;
+          }
+        `}</style>
+      </>
+  );
+}
